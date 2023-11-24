e7238cd3659b299de634aca0b593263d09074377
Frerich Raabe <frerich.raabe@betterdoc.de>
2023-11-24 11:32:14 +0100

    Tweak build script

--- build.sh
+++ build.sh
@@ -4,15 +4,12 @@ set -o errexit
 
 export MIX_ENV=prod
 
-# Initial setup
+mix assets.setup
 mix deps.get --only prod
 mix ecto.setup
 mix compile
-
-# Compile assets
-mix assets.setup
 mix assets.deploy
 
 # Build the release and overwrite the existing release directory
-MIX_ENV=prod mix release --overwrite
+mix release --overwrite
 
