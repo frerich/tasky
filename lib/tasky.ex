b55c446611468617225c4b806ff31272f5170947
Frerich Raabe <frerich.raabe@betterdoc.de>
2023-11-24 13:06:37 +0100

    Show build hash

--- lib/tasky.ex
+++ lib/tasky.ex
@@ -6,4 +6,8 @@ defmodule Tasky do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @build_hash System.cmd("git", ["rev-parse", "--short", "HEAD"]) |> elem(0) |> String.trim()
+
+  def build_hash(), do: @build_hash
 end
