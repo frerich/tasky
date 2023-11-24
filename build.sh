9b6a2dc3c2936b4f864f87e2093e6be8c75d7b44
Frerich Raabe <frerich.raabe@betterdoc.de>
2023-11-24 11:22:23 +0100

    More work on build script

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 # exit on error
 set -o errexit
 
+export MIX_ENV=prod
+
 # Initial setup
 mix deps.get --only prod
 mix ecto.setup
-mix assets.setup
-MIX_ENV=prod mix compile
+mix compile
 
 # Compile assets
+mix assets.setup
 mix assets.deploy
 
 # Build the release and overwrite the existing release directory
